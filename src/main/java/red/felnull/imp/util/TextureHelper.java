2f5219e7661a0f5fd785ce57143db62def9d856a
MORIMORI <morimori.0317@outlook.jp>
2020-07-22 15:43:43 +0900

    ンアッー

--- src/main/java/red/felnull/imp/util/TextureHelper.java
+++ src/main/java/red/felnull/imp/util/TextureHelper.java
@@ -1,22 +1,16 @@
 package red.felnull.imp.util;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture.Type;
-
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.texture.DynamicTexture;
-import net.minecraft.client.renderer.texture.NativeImage;
 import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
 import red.felnull.imp.IamMusicPlayer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TextureHelper {
     private static Minecraft mc = Minecraft.getInstance();
@@ -40,8 +34,11 @@ public class TextureHelper {
         return faselocation;
     }
 
-    public static ResourceLocation getPictueLocation(String uuid) {
+    private static ResourceLocation noalbumimage = new ResourceLocation(IamMusicPlayer.MODID, "textures/gui/no_album_image.png");
 
+    public static ResourceLocation getPictueLocation(String uuid) {
+        return noalbumimage;
+        /*
         if (pictuers.containsKey(uuid)) {
             return pictuers.get(uuid);
         }
@@ -69,6 +66,8 @@ public class TextureHelper {
             pictuers.put(uuid, lodinglocation);
         }
         return lodinglocation;
+
+         */
     }
 
     public static boolean isImageNotExists(String uuid) {
