bba82b7d86691c28d3a5f5b0a778af7874b9565c
MORIMORI <morimori.0317@outlook.jp>
2020-07-22 15:37:18 +0900

    ん？

--- src/main/java/red/felnull/imp/item/CassetteTapeItem.java
+++ src/main/java/red/felnull/imp/item/CassetteTapeItem.java
@@ -1,21 +1,13 @@
 package red.felnull.imp.item;
 
 import net.minecraft.client.renderer.model.IBakedModel;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import red.felnull.imp.client.renderer.item.CassetteItemRenderer;
 import red.felnull.imp.client.renderer.model.CassetteBakedModel;
-import red.felnull.imp.sound.SoundData;
 import red.felnull.imp.util.SoundHelper;
-
-import javax.annotation.Nullable;
-import java.util.List;
 
 public class CassetteTapeItem extends Item {
 
@@ -37,13 +29,13 @@ public class CassetteTapeItem extends Item {
         return befre;
     }
 
-
-    @OnlyIn(Dist.CLIENT)
-    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip,
-                               ITooltipFlag flagIn) {
-        SoundData.addSoundDataTooltip(stack, tooltip);
-    }
-
+    /*
+        @OnlyIn(Dist.CLIENT)
+        public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip,
+                                   ITooltipFlag flagIn) {
+            SoundData.addSoundDataTooltip(stack, tooltip);
+        }
+    */
     public ITextComponent getDisplayName(ItemStack stack) {
         return SoundHelper.isWritedSound(stack)
                 ? new TranslationTextComponent("item.iammusicplayer.cassette_tape.written",
