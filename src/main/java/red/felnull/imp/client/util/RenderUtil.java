86f33de83df04ac55892d065ac4dc2b9a596f0c7
MORIMORI <morimori.0317@outlook.jp>
2020-09-22 03:06:56 +0900

    PlayImageString ?

--- src/main/java/red/felnull/imp/client/util/RenderUtil.java
+++ src/main/java/red/felnull/imp/client/util/RenderUtil.java
@@ -2,19 +2,18 @@ package red.felnull.imp.client.util;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.IFormattableTextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import red.felnull.imp.IamMusicPlayer;
-import red.felnull.imp.client.gui.screen.MusicSharingDeviceScreen;
 import red.felnull.imp.data.IMPWorldData;
 import red.felnull.imp.musicplayer.PlayImage;
 import red.felnull.otyacraftengine.client.util.IKSGRenderUtil;
 import red.felnull.otyacraftengine.client.util.IKSGTextureUtil;
 import red.felnull.otyacraftengine.util.IKSGColorUtil;
 import red.felnull.otyacraftengine.util.IKSGMath;
-import red.felnull.otyacraftengine.util.IKSGStyles;
 
 import java.util.Random;
 
@@ -43,21 +42,7 @@ public class RenderUtil {
                 IKSGRenderUtil.guiBindAndBlit(location, matrix, x + xz, y + yz, 0, 0, xsize, ysize, xsize, ysize);
                 break;
             case STRING:
-                Random r = new Random(IKSGMath.convertStringToInteger(image.getName()));
-                int strColor = r.nextInt(maxColor);
-                int[] backColor = IKSGColorUtil.convertRGBFromColorCode(r.nextInt(maxColor));
-                RenderSystem.pushMatrix();
-                RenderSystem.color4f((float) backColor[0] / 255f, (float) backColor[1] / 255f, (float) backColor[2] / 255f, 1.0F);
-                IKSGRenderUtil.guiBindAndBlit(STRING_PLAYIMAGE, matrix, x, y + upOver, 0, upOver, size, size - downOver - upOver, size, size);
-                RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-                RenderSystem.popMatrix();
-                FontRenderer ft = IamMusicPlayer.proxy.getMinecraft().fontRenderer;
-                IFormattableTextComponent stc = IKSGStyles.withStyle(new StringTextComponent(image.getName()), MusicSharingDeviceScreen.fontStyle);
-                float brit = (float) (size - 4) / (float) ft.func_238414_a_(stc);
-                IKSGRenderUtil.matrixPush(matrix);
-                IKSGRenderUtil.matrixScalf(matrix, brit);
-                IKSGRenderUtil.drawCenterString(ft, matrix, stc, (int) ((x + size / 2) / brit), (int) ((y + size / 2) / brit) - (int) (2.5f * brit), 0);
-                IKSGRenderUtil.matrixPop(matrix);
+                drwPlayImageString(matrix, image.getName(), x, y, size, upOver, downOver);
                 break;
             case PLAYERFACE:
                 IKSGRenderUtil.guiBindAndBlit(IKSGTextureUtil.getPlayerSkinTexture(image.getName()), matrix, x, y, size, size, size, size, size * 8, size * 8);
@@ -78,21 +63,7 @@ public class RenderUtil {
                 IKSGRenderUtil.guiBindAndBlit(location, matrix, x + xz, y + yz, 0, 0, xsize, ysize, xsize, ysize);
                 break;
             case STRING:
-                Random r = new Random(IKSGMath.convertStringToInteger(image.getName()));
-                int strColor = r.nextInt(maxColor);
-                int[] backColor = IKSGColorUtil.convertRGBFromColorCode(r.nextInt(maxColor));
-                RenderSystem.pushMatrix();
-                RenderSystem.color4f((float) backColor[0] / 255f, (float) backColor[1] / 255f, (float) backColor[2] / 255f, 1.0F);
-                IKSGRenderUtil.guiBindAndBlit(STRING_PLAYIMAGE, matrix, x, y, 0, 0, size, size, size, size);
-                RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-                RenderSystem.popMatrix();
-                FontRenderer ft = IamMusicPlayer.proxy.getMinecraft().fontRenderer;
-                IFormattableTextComponent stc = IKSGStyles.withStyle(new StringTextComponent(image.getName()), MusicSharingDeviceScreen.fontStyle);
-                float brit = (float) (size - 4) / (float) ft.func_238414_a_(stc);
-                IKSGRenderUtil.matrixPush(matrix);
-                IKSGRenderUtil.matrixScalf(matrix, brit);
-                IKSGRenderUtil.drawCenterString(ft, matrix, stc, (int) ((x + size / 2) / brit), (int) ((y + size / 2) / brit) - (int) (2.5f * brit), 0);
-                IKSGRenderUtil.matrixPop(matrix);
+                drwPlayImageString(matrix, image.getName(), x, y, size, upOver, downOver);
                 break;
             case PLAYERFACE:
                 IKSGRenderUtil.guiBindAndBlit(IKSGTextureUtil.getPlayerSkinTexture(image.getName()), matrix, x, y, size, size, size, size, size * 8, size * 8);
@@ -100,4 +71,29 @@ public class RenderUtil {
                 break;
         }
     }
+
+    private static void drwPlayImageString(MatrixStack matrix, String str, int x, int y, int size, int upOver, int downOver) {
+        Random r = new Random(IKSGMath.convertStringToInteger(str));
+        int[] backColor = IKSGColorUtil.convertRGBFromColorCode(r.nextInt(maxColor));
+        RenderSystem.pushMatrix();
+        RenderSystem.color4f((float) backColor[0] / 255f, (float) backColor[1] / 255f, (float) backColor[2] / 255f, 1.0F);
+        IKSGRenderUtil.guiBindAndBlit(STRING_PLAYIMAGE, matrix, x, y + upOver, 0, upOver, size, size - downOver - upOver, size, size);
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.popMatrix();
+
+        Minecraft mc = IamMusicPlayer.proxy.getMinecraft();
+        FontRenderer ft = mc.fontRenderer;
+
+        IFormattableTextComponent comp = new StringTextComponent(str);
+        int stWidthSize = ft.func_238414_a_(comp);
+        float baritu = (float) (size - 3) / (float) stWidthSize;
+        float yzure = (size - ft.FONT_HEIGHT * baritu) / 2;
+
+        IKSGRenderUtil.matrixPush(matrix);
+        IKSGRenderUtil.matrixScalf(matrix, baritu);
+        IKSGRenderUtil.drawCenterString(ft, matrix, comp, (int) ((float) (x + size / 2) / baritu), (int) ((float) (y + yzure) / baritu), 0);
+        IKSGRenderUtil.matrixPop(matrix);
+
+    }
+
 }
