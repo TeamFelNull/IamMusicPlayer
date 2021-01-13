a8e7b23700328fe8021356f2a548e833a45986da
MORIMORI0317 <morimori.0317@outlook.jp>
2021-01-13 20:42:58 +0900

    Fix FX

--- src/main/java/red/felnull/imp/client/util/FileUtils.java
+++ src/main/java/red/felnull/imp/client/util/FileUtils.java
@@ -1,29 +1,52 @@
 package red.felnull.imp.client.util;
 
 import java.io.File;
+import java.lang.reflect.Method;
 
 public class FileUtils {
     public static void openFileChoser(String title, OpenFile op) {
-      /*  try {
-            javafx.stage.FileChooser fc = new javafx.stage.FileChooser();
+        try {
+         /*   javafx.stage.FileChooser fc = new javafx.stage.FileChooser();
             fc.setTitle(title);
             com.sun.javafx.application.PlatformImpl.startup(() -> {
                 File file = fc.showOpenDialog(null);
                 op.openFile(file);
+            });*/
+            Class<?> fcClass = Class.forName("javafx.stage.FileChooser");
+            Object fcIns = fcClass.newInstance();
+
+            Method stMethod = fcClass.getDeclaredMethod("setTitle", String.class);
+            stMethod.invoke(fcIns, "A-ikiso");
+
+            Class<?> piClass = Class.forName("com.sun.javafx.application.PlatformImpl");
+            Method suMethod = piClass.getDeclaredMethod("startup", Runnable.class);
+
+            suMethod.invoke(fcIns, (Runnable) () -> {
+                try {
+                    Class<?> wdClass = Class.forName("javafx.stage.Window");
+                    Method sodMethod = fcClass.getDeclaredMethod("showOpenDialog", wdClass);
+                    Object nullble = null;
+                    File file = (File) sodMethod.invoke(fcIns, nullble);
+                    op.openFile(file);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             });
-        } catch (NoClassDefFoundError ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
-        }*/
+        }
     }
 
     public static boolean isCanOpenFileChooser() {
-    /*    try {
-            javafx.stage.FileChooser fc = new javafx.stage.FileChooser();
+        try {
+            //     javafx.stage.FileChooser fc = new javafx.stage.FileChooser();
+            Class<?> fcClass = Class.forName("javafx.stage.FileChooser");
+            Class<?> piClass = Class.forName("com.sun.javafx.application.PlatformImpl");
+            Class<?> wdClass = Class.forName("javafx.stage.Window");
             return true;
-        } catch (NoClassDefFoundError ex) {
+        } catch (Exception ex) {
             return false;
-        }*/
-        return false;
+        }
     }
 
     public interface OpenFile {
