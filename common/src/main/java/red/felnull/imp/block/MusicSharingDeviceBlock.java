2966e624e7b0d92c7d3dee637ade3d16f48dc471
MORIMORI0317 <morimori.0317@outlook.jp>
2021-04-20 18:25:58 +0900

    tick event

--- common/src/main/java/red/felnull/imp/block/MusicSharingDeviceBlock.java
+++ common/src/main/java/red/felnull/imp/block/MusicSharingDeviceBlock.java
@@ -1,10 +1,15 @@
 package red.felnull.imp.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
+import red.felnull.imp.block.shape.MusicSharingDeviceShape;
 import red.felnull.imp.blockentity.IMPBlockEntitys;
 import red.felnull.imp.blockentity.MusicSharingDeviceBlockEntity;
 
@@ -22,5 +27,22 @@ public class MusicSharingDeviceBlock extends IMPEquipmentBaseBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new MusicSharingDeviceBlockEntity(blockPos, blockState);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        Direction direction = blockState.getValue(IMPEquipmentBaseBlock.FACING);
+        switch (direction) {
+            case NORTH:
+                return MusicSharingDeviceShape.NORTH_AXIS_AABB;
+            case SOUTH:
+                return MusicSharingDeviceShape.SOUTH_AXIS_AABB;
+            case EAST:
+                return MusicSharingDeviceShape.EAST_AXIS_AABB;
+            case WEST:
+                return MusicSharingDeviceShape.WEST_AXIS_AABB;
+            default:
+                return MusicSharingDeviceShape.NORTH_AXIS_AABB;
+        }
     }
 }
---
+++ common/src/main/java/red/felnull/imp/block/shape/MusicSharingDeviceShape.java
@@ -0,0 +1,52 @@
+package red.felnull.imp.block.shape;
+
+import net.minecraft.core.Direction;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import red.felnull.otyacraftengine.util.IKSGVoxelShapeUtil;
+
+public class MusicSharingDeviceShape {
+    private static final VoxelShape NORTH_PART1 = IKSGVoxelShapeUtil.makeBox(6.0d, 0.0d, 2.0d, 15.0d, 0.4d, 6.0d);
+    private static final VoxelShape NORTH_PART2 = IKSGVoxelShapeUtil.makeBox(7.93942d, 0.0d, 7.93458d, 13.01784d, 0.5d, 10.97d);
+    private static final VoxelShape NORTH_PART3 = IKSGVoxelShapeUtil.makeBox(10.0d, 0.47587d, 8.46954d, 11.0d, 4.09091d, 9.46954d);
+    private static final VoxelShape NORTH_PART4 = IKSGVoxelShapeUtil.makeBox(10.0d, 2.0d, 9.75d, 11.0d, 2.25d, 10.0d);
+    private static final VoxelShape NORTH_PART5 = IKSGVoxelShapeUtil.makeBox(1.0d, 0.1d, 8.0d, 4.0d, 11.1d, 15.0d);
+    private static final VoxelShape NORTH_PART6 = IKSGVoxelShapeUtil.makeBox(1.0d, 0.0d, 1.0d, 5.0d, 0.1d, 7.0d);
+    private static final VoxelShape NORTH_PART7 = IKSGVoxelShapeUtil.makeBox(7.0d, 0.1d, 11.5d, 14.0d, 1.6d, 15.5d);
+    private static final VoxelShape NORTH_PART8 = IKSGVoxelShapeUtil.makeBox(1.0d, 0.0d, 8.0d, 2.0d, 0.1d, 9.0d);
+    private static final VoxelShape NORTH_PART9 = IKSGVoxelShapeUtil.makeBox(1.0d, 0.0d, 14.0d, 2.0d, 0.1d, 15.0d);
+    private static final VoxelShape NORTH_PART10 = IKSGVoxelShapeUtil.makeBox(3.0d, 0.0d, 14.0d, 4.0d, 0.1d, 15.0d);
+    private static final VoxelShape NORTH_PART11 = IKSGVoxelShapeUtil.makeBox(7.0d, 0.0d, 14.5d, 8.0d, 0.1d, 15.5d);
+    private static final VoxelShape NORTH_PART12 = IKSGVoxelShapeUtil.makeBox(7.0d, 0.0d, 11.5d, 8.0d, 0.1d, 12.5d);
+    private static final VoxelShape NORTH_PART13 = IKSGVoxelShapeUtil.makeBox(13.0d, 0.0d, 11.5d, 14.0d, 0.1d, 12.5d);
+    private static final VoxelShape NORTH_PART14 = IKSGVoxelShapeUtil.makeBox(13.0d, 0.0d, 14.5d, 14.0d, 0.1d, 15.5d);
+    private static final VoxelShape NORTH_PART15 = IKSGVoxelShapeUtil.makeBox(3.0d, 0.0d, 8.0d, 4.0d, 0.1d, 9.0d);
+    private static final VoxelShape NORTH_PART16 = IKSGVoxelShapeUtil.makeBox(2.9d, 10.0d, 7.9d, 3.7d, 10.8d, 8.0d);
+    private static final VoxelShape NORTH_PART17 = IKSGVoxelShapeUtil.makeBox(9.5d, 2.1d, 12.5d, 10.0d, 2.6d, 13.0d);
+    private static final VoxelShape NORTH_PART18 = IKSGVoxelShapeUtil.makeBox(10.0d, 2.6d, 12.5d, 10.5d, 3.1d, 13.0d);
+    private static final VoxelShape NORTH_PART19 = IKSGVoxelShapeUtil.makeBox(10.0d, 3.1d, 13.0d, 10.5d, 3.6d, 13.5d);
+    private static final VoxelShape NORTH_PART20 = IKSGVoxelShapeUtil.makeBox(9.5d, 2.6d, 13.0d, 10.0d, 3.1d, 13.5d);
+    private static final VoxelShape NORTH_PART21 = IKSGVoxelShapeUtil.makeBox(11.0d, 2.6d, 13.0d, 11.5d, 3.1d, 13.5d);
+    private static final VoxelShape NORTH_PART22 = IKSGVoxelShapeUtil.makeBox(10.5d, 2.6d, 12.5d, 11.0d, 3.1d, 13.0d);
+    private static final VoxelShape NORTH_PART23 = IKSGVoxelShapeUtil.makeBox(11.0d, 2.1d, 12.5d, 11.5d, 2.6d, 13.0d);
+    private static final VoxelShape NORTH_PART24 = IKSGVoxelShapeUtil.makeBox(10.5d, 3.1d, 13.0d, 11.0d, 3.6d, 13.5d);
+    private static final VoxelShape NORTH_PART25 = IKSGVoxelShapeUtil.makeBox(10.25d, 3.6d, 13.25d, 10.75d, 4.6d, 13.75d);
+    private static final VoxelShape NORTH_PART26 = IKSGVoxelShapeUtil.makeBox(11.0d, 2.1d, 14.0d, 11.5d, 2.6d, 14.5d);
+    private static final VoxelShape NORTH_PART27 = IKSGVoxelShapeUtil.makeBox(10.5d, 2.6d, 14.0d, 11.0d, 3.1d, 14.5d);
+    private static final VoxelShape NORTH_PART28 = IKSGVoxelShapeUtil.makeBox(10.5d, 3.1d, 13.5d, 11.0d, 3.6d, 14.0d);
+    private static final VoxelShape NORTH_PART29 = IKSGVoxelShapeUtil.makeBox(11.0d, 2.6d, 13.5d, 11.5d, 3.1d, 14.0d);
+    private static final VoxelShape NORTH_PART30 = IKSGVoxelShapeUtil.makeBox(10.0d, 2.6d, 14.0d, 10.5d, 3.1d, 14.5d);
+    private static final VoxelShape NORTH_PART31 = IKSGVoxelShapeUtil.makeBox(10.0d, 3.1d, 13.5d, 10.5d, 3.6d, 14.0d);
+    private static final VoxelShape NORTH_PART32 = IKSGVoxelShapeUtil.makeBox(9.5d, 2.1d, 14.0d, 10.0d, 2.6d, 14.5d);
+    private static final VoxelShape NORTH_PART33 = IKSGVoxelShapeUtil.makeBox(9.5d, 2.6d, 13.5d, 10.0d, 3.1d, 14.0d);
+    private static final VoxelShape NORTH_PART34 = IKSGVoxelShapeUtil.makeBox(9.5d, 1.6d, 12.5d, 11.5d, 2.1d, 14.5d);
+    private static final VoxelShape NORTH_PART35 = IKSGVoxelShapeUtil.makeBox(14.0d, 0.6d, 12.8d, 14.05d, 1.3d, 14.2d);
+    private static final VoxelShape NORTH_PART36 = IKSGVoxelShapeUtil.makeBox(4.99918d, 1.49787d, 7.60933d, 16.03763d, 9.51698d, 8.19435d);
+    private static final VoxelShape NORTH_PART37 = IKSGVoxelShapeUtil.makeBox(6.75d, 3.0d, 8.25d, 14.25d, 7.75d, 9.25d);
+
+    public static final VoxelShape NORTH_AXIS_AABB = Shapes.or(NORTH_PART1, NORTH_PART2, NORTH_PART3, NORTH_PART4, NORTH_PART5, NORTH_PART6, NORTH_PART7, NORTH_PART8, NORTH_PART9, NORTH_PART10, NORTH_PART11, NORTH_PART12, NORTH_PART13, NORTH_PART14, NORTH_PART15, NORTH_PART16, NORTH_PART17, NORTH_PART18, NORTH_PART19, NORTH_PART20, NORTH_PART21, NORTH_PART22, NORTH_PART23, NORTH_PART24, NORTH_PART25, NORTH_PART26, NORTH_PART27, NORTH_PART28, NORTH_PART29, NORTH_PART30, NORTH_PART31, NORTH_PART32, NORTH_PART33, NORTH_PART34, NORTH_PART35, NORTH_PART36, NORTH_PART37);
+    public static final VoxelShape SOUTH_AXIS_AABB = IKSGVoxelShapeUtil.rotateBoxDirection(NORTH_AXIS_AABB, Direction.SOUTH);
+    public static final VoxelShape EAST_AXIS_AABB = IKSGVoxelShapeUtil.rotateBoxDirection(NORTH_AXIS_AABB, Direction.EAST);
+    public static final VoxelShape WEST_AXIS_AABB = IKSGVoxelShapeUtil.rotateBoxDirection(NORTH_AXIS_AABB, Direction.WEST);
+
+}
