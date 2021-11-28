a7eea7b979ca600898505cca652e3e4384ad105c
MORIMORI0317 <morimori.0317@outlook.jp>
2021-11-29 00:25:54 +0900

    add boombox shape

--- common/src/main/java/dev/felnull/imp/block/BoomboxBlock.java
+++ common/src/main/java/dev/felnull/imp/block/BoomboxBlock.java
@@ -1,12 +1,22 @@
 package dev.felnull.imp.block;
 
+import dev.felnull.imp.IamMusicPlayer;
 import dev.felnull.imp.blockentity.BoomboxBlockEntity;
+import dev.felnull.otyacraftengine.util.OEVoxelShapeUtil;
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class BoomboxBlock extends IMPBaseEntityBlock {
+    private static final OEVoxelShapeUtil.DirectionVoxelShapes SHAPE = OEVoxelShapeUtil.makeAllDirection(OEVoxelShapeUtil.getShapeFromResource(new ResourceLocation(IamMusicPlayer.MODID, "boombox"), MusicManagerBlock.class));
+    private static final OEVoxelShapeUtil.DirectionVoxelShapes SHAPE_CLOSE = OEVoxelShapeUtil.makeAllDirection(OEVoxelShapeUtil.getShapeFromResource(new ResourceLocation(IamMusicPlayer.MODID, "boombox_close_handle"), MusicManagerBlock.class));
+
+
     protected BoomboxBlock(Properties properties) {
         super(properties);
     }
@@ -15,5 +25,10 @@ public class BoomboxBlock extends IMPBaseEntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new BoomboxBlockEntity(blockPos, blockState);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return SHAPE.getShape(blockState.getValue(FACING));
     }
 }
--- common/src/main/java/dev/felnull/imp/item/TestSoundItem.java
+++ common/src/main/java/dev/felnull/imp/item/TestSoundItem.java
@@ -1,13 +1,10 @@
 package dev.felnull.imp.item;
 
-import com.sedmelluq.discord.lavaplayer.source.http.HttpAudioSourceManager;
-import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
-import dev.felnull.imp.client.music.MusicEngine;
-import dev.felnull.imp.client.util.LavaPlayerUtil;
-import dev.felnull.imp.music.MusicPlayManager;
-import dev.felnull.imp.music.MusicPlaybackInfo;
-import dev.felnull.imp.music.resource.MusicSource;
-import net.minecraft.network.chat.TextComponent;
+import com.google.gson.Gson;
+import dev.felnull.imp.IamMusicPlayer;
+import dev.felnull.otyacraftengine.util.OEVoxelShapeUtil;
+import dev.felnull.otyacraftengine.vsg.TentativeVoxelShapeGenerator;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -15,7 +12,9 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
-import java.util.UUID;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class TestSoundItem extends Item {
 
@@ -28,7 +27,7 @@ public class TestSoundItem extends Item {
         ItemStack itemStack = player.getItemInHand(interactionHand);
 
         if (level.isClientSide()) {
-            UUID id = UUID.randomUUID();
+           /* UUID id = UUID.randomUUID();
             MusicEngine me = MusicEngine.getInstance();
             var apm = LavaPlayerUtil.createAudioPlayerManager();
             apm.registerSourceManager(new YoutubeAudioSourceManager());
@@ -42,7 +41,13 @@ public class TestSoundItem extends Item {
                     player.displayClientMessage(new TextComponent(result + ":" + time + ":" + retry), false);
                     me.playMusicPlayer(id, 0);
                 });
-            }
+            }*/
+            /*var shpe = TentativeVoxelShapeGenerator.generate(OEVoxelShapeUtil.getShapeFromResource(new ResourceLocation(IamMusicPlayer.MODID, "boombox/boombox_fast_handle_shape")), OEVoxelShapeUtil.getShapeFromResource(new ResourceLocation(IamMusicPlayer.MODID, "boombox/boombox_fast_shape")));
+            try {
+                Files.writeString(Paths.get("shape.json"), new Gson().toJson(shpe));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }*/
         }
         return InteractionResultHolder.sidedSuccess(itemStack, level.isClientSide);
     }
